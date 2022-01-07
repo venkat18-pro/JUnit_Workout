e5fde48314979e8f6e81e67f5a8894bb23249f72
Venkatesh <viv7012@gmail.com>
2022-01-07 08:46:36 +0530

    Assert method added..

--- src/test/java/com/junit/demo/test/JunitMain.java
+++ src/test/java/com/junit/demo/test/JunitMain.java
@@ -5,32 +5,34 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+
 public class JunitMain {
-	static final WebDriver driver = new ChromeDriver();
-	final Date d = new Date();
+	static WebDriver driver; 
+	Date d = new Date();
 	
 	@BeforeClass
 	public static void launchBrowser() {
 		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.get("https://facebook.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
 	@Before
-	public void beforeTime() {
-		
-		System.out.println(d.getTime());
-
+	public void beforeTime() {	
+		System.out.println(d);
 	}
 	
 	@Test
@@ -40,10 +42,16 @@ public class JunitMain {
 		driver.findElement(By.name("login")).click();
 	}
 	
+	@Test
+	public void test2() {
+		WebElement error = driver.findElement(By.xpath("//div[contains(text(), 'incorrect')]"));
+		String errorMeg = error.getText();
+		Assert.assertTrue( "Error Message Display: ", errorMeg.contains("incorrect"));
+	}
+	
 	@After
 	public void afterTime() {
-		System.out.println(d.getTime());
-
+		System.out.println(d);
 	}
 	
 	@AfterClass
